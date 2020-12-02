dcde665f729d0fc3e9b4f7001697680457e2f6d2
xiaoxuan6 <32762220+xiaoxuan6@users.noreply.github.com>
2020-12-02 18:36:45 +0800

    映射类型

--- app/Console/Commands/Indices.php
+++ app/Console/Commands/Indices.php
@@ -18,10 +18,18 @@ class Indices
             "number_of_replicas" => 0
         ];
     }
-
+    /**
+     * Notes: 映射
+     *
+     * dynamic：
+     *          动态映射（dynamic：true）：动态添加新的字段（或缺省）。
+     *          静态映射（dynamic：false）：忽略新的字段。在原有的映射基础上，当有新的字段时，不会主动的添加新的映射关系，只作为查询结果出现在查询中。（不在映射中设置的字段，不支持搜索）
+     *          严格模式（dynamic： strict）：如果遇到新的字段，就抛出异常。
+     */
     static public function getProperties()
     {
         return [
+            "dynamic" => false,
             "properties" => [
                 "name"    => [
                     "type"            => "text",
